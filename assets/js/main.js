9409e4d847f6fb098211baa5c75104c53f4ff1b0
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-08-24 23:31:28 -0500

    added popup fade in effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,4 +63,20 @@
 					visibleClass: 'navPanel-visible'
 				});
 
+				$(document).on("scroll", function() {
+					var pageTop = $(document).scrollTop();
+					var pageBottom = pageTop + $(window).height();
+					var tags = $(".popup");
+				  
+					for (var i = 0; i < tags.length; i++) {
+					  var tag = tags[i];
+					  if ($(tag).position().top < pageBottom) {
+						$(tag).addClass("visible");
+					  } else {
+						$(tag).removeClass("visible");
+					  }
+					}
+				  });
+
 })(jQuery);
+
