bea40f78821db0cc9d8b7afddcd50ac2bc4d5c35
AllyA94 <102490350+tamuwicys@users.noreply.github.com>
2024-10-09 15:28:03 -0500

    alumni changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	typeWriter();
 	});
+	document.addEventListener('DOMContentLoaded', function() {
+		const text = "Our Alumni";
+		const speed = 100; // Typing speed in milliseconds
+	
+		function typeWriter() {
+			const container = document.getElementById('typewriter-text8');
+			let i = 0;
+	
+			function type() {
+			if (i < text.length) {
+				container.textContent += text.charAt(i);
+				i++;
+				setTimeout(type, speed);
+			}
+			}
+	
+			type();
+		}
+	
+		typeWriter();
+		});
 document.addEventListener('DOMContentLoaded', function() {
 		const text = "About Us";
 		const speed = 100; // Typing speed in milliseconds
